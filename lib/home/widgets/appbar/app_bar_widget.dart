6a580bae2acd17e92a70038059257d521045611e
Rubem <rfsv@ic.ufal.br>
2021-04-19 23:52:46 -0300

    chore: use widgets in app bar widget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/core/app_gradients.dart';
 import 'package:flutter_quiz_app/core/core.dart';
+import 'package:flutter_quiz_app/home/widgets/score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
@@ -8,36 +9,47 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text.rich(
-                      TextSpan(
-                          text: "Olá, ",
-                          style: AppTextStyles.title,
-                          children: [
-                            TextSpan(
-                              text: '@Rubemfsv',
-                              style: AppTextStyles.titleBold,
-                            ),
-                          ]),
-                    ),
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image:
-                                NetworkImage('https://github.com/rubemfsv.png'),
-                          )),
-                    )
-                  ]),
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: '@Rubemfsv',
+                                style: AppTextStyles.titleBold,
+                              ),
+                            ]),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://github.com/rubemfsv.png'),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
