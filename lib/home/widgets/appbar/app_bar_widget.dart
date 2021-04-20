af3cfcbef0647935c25cfe397fe907f62b561fe5
Rubem <rfsv@ic.ufal.br>
2021-04-19 23:24:56 -0300

    feature: create appbar

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/core/app_gradients.dart';
+import 'package:flutter_quiz_app/core/core.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: '@Rubemfsv',
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ]),
+                    ),
+                    Container(
+                      width: 58,
+                      height: 58,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image:
+                                NetworkImage('https://github.com/rubemfsv.png'),
+                          )),
+                    )
+                  ]),
+            ),
+          ),
+        );
+}
