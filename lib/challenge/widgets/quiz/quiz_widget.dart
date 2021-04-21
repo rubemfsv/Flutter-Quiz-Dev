ef933cb14990e281890f3c0dff110435ba959798
Rubem <rfsv@ic.ufal.br>
2021-04-21 17:04:27 -0300

    feature: create quiz  widget

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/core/core.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+        ],
+      ),
+    );
+  }
+}
